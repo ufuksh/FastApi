8ca05bc541a662bac47cf6ff35e1ca12b6cae47a
ufuksh <439504@ogr.ktu.edu.tr>
2024-12-10 16:31:25 +0300

    prod

--- app/backend/models.py
+++ app/backend/models.py
@@ -1,15 +1,15 @@
 # backend/app/models.py
+
 from sqlalchemy import Column, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 import uuid
 from datetime import datetime
 from .database import Base
 
-
 class Student(Base):
     __tablename__ = "students"
 
-    id = Column(SQLUUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()), index=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
@@ -20,7 +20,7 @@ class Student(Base):
 class Teacher(Base):
     __tablename__ = "teachers"
 
-    id = Column(SQLUUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()), index=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
@@ -31,13 +31,13 @@ class Teacher(Base):
 class Schedule(Base):
     __tablename__ = "schedules"
 
-    id = Column(SQLUUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()), index=True)
     title = Column(String, nullable=False)
     description = Column(String, nullable=True)
     start_time = Column(DateTime, nullable=False)
     end_time = Column(DateTime, nullable=False)
-    student_id = Column(SQLUUID(as_uuid=True), ForeignKey("students.id"))
-    teacher_id = Column(SQLUUID(as_uuid=True), ForeignKey("teachers.id"))
+    student_id = Column(String(36), ForeignKey("students.id"))
+    teacher_id = Column(String(36), ForeignKey("teachers.id"))
     created_at = Column(DateTime, default=datetime.utcnow)
 
     student = relationship("Student", back_populates="schedules")
